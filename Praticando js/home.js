000128a4912c5f2a399721762a50d3cc7c47a92c
Max-mcp <duartemaxwell28@gmail.com>
2026-08-21 21:14:04 -0300

    jdjdjdjd

---
+++ Praticando js/home.js
@@ -0,0 +1,57 @@
+const nomeEntrada = document.getElementById("entrada-nome");
+const nomeSaida = document.getElementById("nome-saida");
+const btnTroca = document.getElementById("btn-troca");
+
+btnTroca.addEventListener('click', () => {
+    nomeSaida.textContent = nomeEntrada.value;
+})
+btnTroca.addEventListener('mouseenter', function() {
+    nomeSaida.style.color = "#73030D"
+})
+btnTroca.addEventListener('mouseout', function() {
+    nomeSaida.style.color = "#8C8C8C"
+})
+
+const btnVerde = document.getElementById("btn_verde");
+const btnAmarelo = document.getElementById("btn_amarelo");
+const btnAzul = document.getElementById("btn_azul");
+const caixa = document.getElementById("caixa");
+
+btnVerde.addEventListener('click', function(){
+    caixa.style.backgroundColor = "green"
+})
+btnAmarelo.addEventListener('click', function(){
+    caixa.style.backgroundColor = "yellow"
+})
+btnAzul.addEventListener('click', function(){
+    caixa.style.backgroundColor = "blue"
+})
+// SIMULADOR ADIÇÃO E SUB DE VALOR
+const valorReal = document.getElementById("valor_real");
+let contador = 0;
+document.getElementById("btn_menos").addEventListener('click',
+    () => {
+        contador --;
+        valorReal.textContent=contador;
+
+    });
+document.getElementById("btn_mais").addEventListener('click',
+    () => {
+        contador ++;
+        valorReal.textContent=contador;
+
+    });
+//Adicionar Livro
+const nomelivro = document.getElementById("nome_livro");
+const listaLivros = dcument.getElementById("lista_livros")
+
+document.getElementById("adicionar_livro").addEventListener('click', () => {
+
+const novolivro = document. createElement("li");
+novolivro.textContent = nomelivro. value;
+listaLivros.appendChild(novolivro);
+
+nomelivro
+
+
+})
